4d8739d5f6f803e84b55b1c9200b2d142298c179
Adrien Boucher <adspartan04@gmail.com>
2015-06-12 10:22:15 +0200

    Correction de la stratégie LRU et cache.c

--- src/C/LRU_strategy.c
+++ src/C/LRU_strategy.c
@@ -32,30 +32,17 @@ struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache)
 {
     struct Cache_Block_Header *pbh;
     struct Cache_List *list = (struct Cache_List*)pcache->pstrategy, *tmp_list = list; 
+    
 
     /* On cherche d'abord un bloc invalide */
     if ((pbh = Get_Free_Block(pcache)) != NULL){
 		Cache_List_Move_To_End(tmp_list, pbh);
 		return pbh;
 	}
-    while(tmp_list)
-    {
-		pbh = tmp_list->pheader;
-		
-		if(!pbh)
-			break;
-		
-		if(pbh && !(VALID & pbh->flags))
-		{			
-			Cache_List_Move_To_End(list, pbh);
-			return pbh;
-		}	
-		tmp_list = tmp_list->prev;
-	}
-	
-    /* Pas de bloc invalide trouvé, on prend le 1er	*/
+
     pbh = Cache_List_Remove_First(list);
-    Cache_List_Move_To_End(list, pbh);
+    Cache_List_Append(list, pbh);
+    
     return pbh;
 }
 
@@ -68,7 +55,7 @@ void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pb)
 //! Fonction "réflexe" lors de l'écriture.
 void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pb)
 {
-	Cache_List_Move_To_End((struct Cache_List*)pcache->pstrategy, pb);
+	Cache_List_Move_To_End((struct Cache_List *)pcache->pstrategy, pb);
 }
 
 //! Identification de la stratégie.
--- src/C/cache.c
+++ src/C/cache.c
@@ -11,6 +11,14 @@
 #include "low_cache.h"
 
 
+int CheckSync(struct Cache *pcache)
+{
+	struct Cache_Instrument instru = pcache->instrument;
+	
+	return (!((instru.n_reads+instru.n_writes)%NSYNC) ? 1 : 0); 
+}
+
+
 void Create_Blocks(struct Cache *pcache)
 {
 	int ibcache = 0;
@@ -18,9 +26,9 @@ void Create_Blocks(struct Cache *pcache)
 	for(struct Cache_Block_Header *tmp = pcache->headers; tmp < pcache->headers + pcache->nblocks; tmp++)
 	{
 		struct Cache_Block_Header cbh = {0, 0, 0, NULL};
-		cbh.data = malloc(pcache->nrecords*pcache->recordsz+1);
+		cbh.data = malloc(pcache->blocksz);
 		cbh.ibcache = ibcache++;
-		*tmp = cbh;
+		*tmp = cbh;		
 	}
 	
 	pcache->pfree = pcache->headers;
@@ -31,11 +39,18 @@ struct Cache *Cache_Create(const char *fic, unsigned nblocks, unsigned nrecords,
 {
 	struct Cache *cache = malloc(sizeof(struct Cache));
 	cache->file = (char*)fic;
-	cache->fp = fopen(fic, "r+");
+	
+	if(!(cache->fp = fopen(fic, "w+")))
+	{
+		perror("fopen");
+		exit(-1);
+	}
+	rewind(cache->fp);
+
 	cache->nblocks = nblocks;
 	cache->nrecords = nrecords;
 	cache->recordsz = recordsz;
-	cache->blocksz = sizeof(struct Cache_Block_Header);
+	cache->blocksz = recordsz*nrecords;
 	cache->nderef = nderef;
 	cache->pstrategy = Strategy_Create(cache);
 	
@@ -54,6 +69,8 @@ Cache_Error Cache_Close(struct Cache *pcache)
 	Cache_Sync(pcache);
 	Strategy_Close(pcache);
 	
+	fclose(pcache->fp);
+	
 	for(struct Cache_Block_Header *h = pcache->headers; h < (pcache->headers + pcache->nblocks); h++)
 		free(h->data);
 
@@ -71,20 +88,21 @@ Cache_Error Cache_Sync(struct Cache *pcache)
 		if(h->flags & MODIF)
 		{
 			h->flags &= ~MODIF;
-			fseek(pcache->fp, h->ibfile, SEEK_SET);
+			fseek(pcache->fp, DADDR(pcache, h->ibfile), SEEK_SET);
 			fwrite(h->data, pcache->blocksz, 1, pcache->fp);
 		}
 	}
+	pcache->instrument.n_syncs++;
 	
 	return CACHE_OK;
 }
 
 //! Invalidation du cache.
 Cache_Error Cache_Invalidate(struct Cache *pcache)
-{
+{	
 	for(struct Cache_Block_Header *h = pcache->headers; h < (pcache->headers + pcache->nblocks); h++)
-		h->flags = 0;
-	
+		h->flags &= ~VALID;
+
 	Strategy_Invalidate(pcache);
 	
 	return CACHE_OK;
@@ -93,6 +111,9 @@ Cache_Error Cache_Invalidate(struct Cache *pcache)
 //! Lecture  (à travers le cache).
 Cache_Error Cache_Read(struct Cache *pcache, int irfile, void *precord)
 {
+	if(CheckSync(pcache))
+		Cache_Sync(pcache);
+
 	int ibfile = (int)(irfile/pcache->nrecords);
 	struct Cache_Block_Header *cbh = NULL;
 	
@@ -101,6 +122,7 @@ Cache_Error Cache_Read(struct Cache *pcache, int irfile, void *precord)
 		if(h->ibfile == ibfile && (h->flags & VALID))
 		{
 			cbh = h;
+			pcache->instrument.n_hits++;
 			break;
 		}
 	}
@@ -108,14 +130,26 @@ Cache_Error Cache_Read(struct Cache *pcache, int irfile, void *precord)
 	if(!cbh)
 	{				
 		cbh = Strategy_Replace_Block(pcache);
+		if(!cbh)
+			cbh = pcache->headers;
+
+		
+		if(cbh->flags&MODIF)
+		{
+			fseek(pcache->fp, DADDR(pcache, cbh->ibfile), SEEK_SET);
+			fwrite(cbh->data, pcache->blocksz, 1, pcache->fp);
+		}
+		
 		cbh->ibfile = ibfile;
 		cbh->flags = VALID;
-		fseek(pcache->fp, irfile, SEEK_SET);
-		fread(cbh->data, pcache->recordsz, pcache->nrecords, pcache->fp);
+		fseek(pcache->fp, DADDR(pcache, ibfile), SEEK_SET);
+		fread(cbh->data, pcache->blocksz, 1, pcache->fp);
 	}
-	
+
 	memcpy(precord, ADDR(pcache, irfile, cbh), pcache->recordsz); 
 	
+	pcache->instrument.n_reads++;
+
 	Strategy_Read(pcache, cbh);
 	
 	return CACHE_OK;
@@ -124,6 +158,9 @@ Cache_Error Cache_Read(struct Cache *pcache, int irfile, void *precord)
 //! Écriture (à travers le cache).
 Cache_Error Cache_Write(struct Cache *pcache, int irfile, const void *precord)
 {	
+	if(CheckSync(pcache))
+		Cache_Sync(pcache);
+		
 	int ibfile = (int)(irfile/pcache->nrecords);
 	struct Cache_Block_Header *cbh = NULL;
 	
@@ -131,22 +168,35 @@ Cache_Error Cache_Write(struct Cache *pcache, int irfile, const void *precord)
 	{
 		if(h->ibfile == ibfile && (h->flags & VALID))
 		{
+			pcache->instrument.n_hits++;
 			cbh = h;
 			break;
 		}
 	}
-	
 	if(!cbh)
-	{				
+	{	
+		
 		cbh = Strategy_Replace_Block(pcache);
+		if(!cbh)
+			cbh = pcache->headers;
+		
+		if(cbh->flags&MODIF)
+		{
+			fseek(pcache->fp, DADDR(pcache, cbh->ibfile), SEEK_SET);
+			fwrite(cbh->data, pcache->blocksz, 1, pcache->fp);
+		}
+				
 		cbh->ibfile = ibfile;
 		cbh->flags = (VALID|MODIF);
-		fseek(pcache->fp, irfile, SEEK_SET);
-		fwrite(cbh->data, pcache->recordsz, pcache->nrecords, pcache->fp);
+		fseek(pcache->fp, DADDR(pcache, ibfile), SEEK_SET);
+		fread(cbh->data, pcache->blocksz, 1, pcache->fp);
 	}
 	
-	memcpy(ADDR(pcache, irfile, cbh), precord, pcache->recordsz);
+	memcpy(ADDR(pcache, irfile, cbh), precord, pcache->recordsz);	
+	pcache->instrument.n_writes++;
+	
 	Strategy_Write(pcache, cbh);
+	
 	return CACHE_OK;
 }
 
@@ -154,26 +204,18 @@ Cache_Error Cache_Write(struct Cache *pcache, int irfile, const void *precord)
 //! Résultat de l'instrumentation.
 struct Cache_Instrument *Cache_Get_Instrument(struct Cache *pcache)
 {
-	return &(pcache->instrument);
-}
-
-
-int main(int argc, char** args)
-{	
-	struct Cache *c;
+	struct Cache_Instrument *p = malloc(sizeof(struct Cache_Instrument));
+	p->n_reads = pcache->instrument.n_reads;	
+	p->n_writes = pcache->instrument.n_writes;
+	p->n_hits = pcache->instrument.n_hits;
+	p->n_syncs = pcache->instrument.n_syncs;
+	p->n_deref = pcache->instrument.n_deref;
 	
-	if(!(c = Cache_Create("foo", 30, 10, 10, 0)))
-	{
-		perror("Cache Create");
-		exit(-1);
-	}
+	pcache->instrument.n_syncs = 0;
+	pcache->instrument.n_hits = 0;
+	pcache->instrument.n_reads = 0;
+	pcache->instrument.n_writes = 0;
+	pcache->instrument.n_deref = 0;
 	
-	for(struct Cache_Block_Header *h = c->headers; h < (c->headers + c->nblocks); h++)
-	{
-		printf("'%s'\n", h->data);
-	}
-	
-	printf("CLOSE\n");
-	Cache_Close(c);
-	return EXIT_SUCCESS;
+	return p;
 }
